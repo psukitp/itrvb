444b1369f46af93be013d2ecb42786a3214702e2
Мышкин Даниил <dsmyshkin@asuproject.ru>
2024-01-09 22:57:48 +0500

    add fk to authors

--- 4/db.sql
+++ 4/db.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
 
 CREATE TABLE posts (
     uuid UUID PRIMARY KEY,
-    author_uuid UUID,
+    FOREIGN KEY(author_uuid) REFERENCES users(uuid),
     title TEXT NOT NULL,
     text TEXT NOT NULL
 );
@@ -15,7 +15,7 @@ CREATE TABLE posts (
 CREATE TABLE comments (
     uuid UUID PRIMARY KEY,
     post_uuid UUID,
-    author_uuid UUID,
+    FOREIGN KEY(author_uuid) REFERENCES users(uuid),
     text TEXT NOT NULL,
     FOREIGN KEY(post_uuid) REFERENCES posts(uuid)
 );
