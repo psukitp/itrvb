1469a8dd4bc87750281f318b2bf85281dfe1bf46
Мышкин Даниил <dsmyshkin@asuproject.ru>
2024-01-10 00:16:17 +0500

    laba 5 done

---
+++ 5/src/Model/Person.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace my\Model;
+
+use my\Model\Name;
+
+class Person
+{
+    public function __construct(
+        private Name $name,
+        private \DateTimeImmutable $regiseredOn
+    ) {
+
+    }
+
+    public function __toString(): string
+    {
+        return $this->name . ' (since' . $this->regiseredOn->format('Y-m-d') . ')';
+    }
+}
